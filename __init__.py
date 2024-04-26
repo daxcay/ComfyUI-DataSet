38bf3708ec140eae814f91b3afd57dd9323e7456
daxcay <daxtoncaylor@gmail.com>
2024-04-26 19:28:52 +0530

    Initial Commit

---
+++ __init__.py
@@ -0,0 +1,35 @@
+# By Daxton Caylor
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to
+# deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""
+@author: Daxton Caylor
+@title: ComfyUI-DRMN
+@nickname: ComfyUI-DRMN
+@description: Data Research And Manipulators Nodes for Model Trainers, Artists, Designers and Animators.
+"""
+
+from .classes.DRMN_TXTFileSaver import N_CLASS_MAPPINGS as TXTFileSaverMappings, N_DISPLAY_NAME_MAPPINGS as TXTFileSaverNameMappings
+from .classes.DRMN_TagManipulatorByImageNames import N_CLASS_MAPPINGS as TagManipulatorByImageNamesMappings, N_DISPLAY_NAME_MAPPINGS as TagManipulatorByImageNamesNameMappings
+from .classes.DRMN_CaptionVisualizer import N_CLASS_MAPPINGS as CaptionVisualizerMappings, N_DISPLAY_NAME_MAPPINGS as CaptionVisualizerNameMappings
+
+NODE_CLASS_MAPPINGS = {}
+NODE_CLASS_MAPPINGS.update(TXTFileSaverMappings)
+NODE_CLASS_MAPPINGS.update(TagManipulatorByImageNamesMappings)
+NODE_CLASS_MAPPINGS.update(CaptionVisualizerMappings)
+
+NODE_DISPLAY_NAME_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS.update(TXTFileSaverNameMappings)
+NODE_DISPLAY_NAME_MAPPINGS.update(TagManipulatorByImageNamesNameMappings)
+NODE_DISPLAY_NAME_MAPPINGS.update(CaptionVisualizerNameMappings)
+
+WEB_DIRECTORY = "./web"
---
+++ classes/DRMN_TXTFileSaver.py
@@ -0,0 +1,75 @@
+import os
+
+def save_file(filename, output_dir, content, mode='SaveNew'):
+    os.makedirs(output_dir, exist_ok=True)
+    file_path = os.path.join(output_dir, filename)
+    
+    if mode == 'SaveNew':
+        counter = 0
+        while os.path.exists(file_path):
+            counter += 1
+            file_path = os.path.join(output_dir, f"{os.path.splitext(filename)[0]}_{counter}{os.path.splitext(filename)[1]}")
+    elif mode == 'Merge' and os.path.exists(file_path):
+        with open(file_path, 'a') as file:
+            file.write(content)
+        print(f"Content appended successfully to {file_path}")
+        return
+    elif mode == 'Overwrite' and os.path.exists(file_path):
+        os.remove(file_path)
+    elif mode == 'MergeAndSaveNew' and os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            existing_content = file.read()
+        content = existing_content + content
+        counter = 0
+        while os.path.exists(file_path):
+            counter += 1
+            file_path = os.path.join(output_dir, f"{os.path.splitext(filename)[0]}_{counter}{os.path.splitext(filename)[1]}")
+    
+    with open(file_path, 'w') as file:
+        file.write(content)
+    print(f"File saved successfully at {file_path}")
+
+class DRMN_TXTFileSaver:
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "content": ("STRING",{"forceInput": True}),
+                "file_name": ("STRING",{"forceInput": True}),
+                "directory": ("STRING", {"default": "directory path"}),
+                "mode": (['Merge','Overwrite','SaveNew','MergeAndSaveNew'],),
+            },
+        }
+
+    # INPUT_IS_LIST = True
+    RETURN_TYPES = ()
+    FUNCTION = "SaveIT"
+    OUTPUT_NODE = True
+
+    CATEGORY = "RAFTOOLS"
+
+    def SaveIT(self, content, file_name, directory, mode):
+        try:
+
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+            save_file(f"{file_name}.txt", directory, content, mode)
+
+        except Exception as e:
+            print(f"Error saving: {e}")
+
+        return ()
+
+
+N_CLASS_MAPPINGS = {
+    "DRMN_TXTFileSaver": DRMN_TXTFileSaver,
+}
+
+N_DISPLAY_NAME_MAPPINGS = {
+    "DRMN_TXTFileSaver": "DRMN_TXTFileSaver",
+}
